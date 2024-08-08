b63141b64555e7550a27a49228d1c8b1d9f36cf1
Thiago Ferreira <thiagomf712@gmail.com>
2024-08-08 11:27:17 -0300

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './global.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+}
