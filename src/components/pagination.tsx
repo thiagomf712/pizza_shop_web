da91092eba4061ff15a0b824ab37998926df6f19
Thiago Ferreira <thiagomf712@gmail.com>
2024-08-09 14:36:33 -0300

    Layout Order Details

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -32,26 +32,26 @@ export function Pagination({
         </div>
 
         <div className="flex items-center gap-2">
-          <Button variant="outline" className="h-8 w-8 p-0">
-            <ChevronsLeft className="h-4 w-4" />
+          <Button variant="outline" className="size-8 p-0">
+            <ChevronsLeft className="size-4" />
 
             <span className="sr-only">Primeira página</span>
           </Button>
 
-          <Button variant="outline" className="h-8 w-8 p-0">
-            <ChevronLeft className="h-4 w-4" />
+          <Button variant="outline" className="size-8 p-0">
+            <ChevronLeft className="size-4" />
 
             <span className="sr-only">Página anterior</span>
           </Button>
 
-          <Button variant="outline" className="h-8 w-8 p-0">
-            <ChevronRight className="h-4 w-4" />
+          <Button variant="outline" className="size-8 p-0">
+            <ChevronRight className="size-4" />
 
             <span className="sr-only">Próxima página</span>
           </Button>
 
-          <Button variant="outline" className="h-8 w-8 p-0">
-            <ChevronsRight className="h-4 w-4" />
+          <Button variant="outline" className="size-8 p-0">
+            <ChevronsRight className="size-4" />
 
             <span className="sr-only">Última página</span>
           </Button>
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,16 +1,26 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="size-3" />
+
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         dasd213df435gd
@@ -18,7 +28,7 @@ export function OrderTableRow() {
       <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
       <TableCell>
         <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="size-2 rounded-full bg-slate-400" />
           <span className="font-medium text-muted-foreground">Pendente</span>
         </div>
       </TableCell>
@@ -26,13 +36,13 @@ export function OrderTableRow() {
       <TableCell className="font-medium">R$ 119,50</TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
-          <ArrowRight className="mr-2 h-3 w-3" />
+          <ArrowRight className="mr-2 size-3" />
           Aprovar
         </Button>
       </TableCell>
       <TableCell>
         <Button variant="ghost" size="xs">
-          <X className="mr-2 h-3 w-3" />
+          <X className="mr-2 size-3" />
           Cancelar
         </Button>
       </TableCell>
