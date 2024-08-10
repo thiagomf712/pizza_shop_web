b3e91b4d5cf27e8c473a38c689e3d53283a3516c
Thiago Ferreira <thiagomf712@gmail.com>
2024-08-10 10:03:01 -0300

    Order list with pagination and filters

---
+++ src/pages/app/orders/order-status.tsx
@@ -0,0 +1,34 @@
+import { twMerge } from 'tailwind-merge'
+
+type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+interface OrderStatusProps {
+  status: OrderStatus
+}
+
+const orderStatusMap: Record<OrderStatus, { label: string; color: string }> = {
+  pending: { label: 'Pendente', color: 'bg-slate-400' },
+  canceled: { label: 'Cancelado', color: 'bg-rose-500' },
+  delivered: { label: 'Entregue', color: 'bg-emerald-500' },
+  delivering: { label: 'Em entrega', color: 'bg-amber-500' },
+  processing: { label: 'Em preparo', color: 'bg-amber-500' },
+}
+
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span
+        className={twMerge('size-2 rounded-full', orderStatusMap[status].color)}
+      />
+
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status].label}
+      </span>
+    </div>
+  )
+}
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
@@ -5,8 +7,19 @@ import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
 
 import { OrderDetails } from './order-details'
+import { OrderStatus } from './order-status'
 
-export function OrderTableRow() {
+interface OrderTableRowProps {
+  order: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -22,18 +35,31 @@ export function OrderTableRow() {
           <OrderDetails />
         </Dialog>
       </TableCell>
+
       <TableCell className="font-mono text-xs font-medium">
-        dasd213df435gd
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
+        })}
+      </TableCell>
+
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="size-2 rounded-full bg-slate-400" />
-          <span className="font-medium text-muted-foreground">Pendente</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
-      <TableCell className="font-medium">Thiago Mendes Ferreira</TableCell>
-      <TableCell className="font-medium">R$ 119,50</TableCell>
+
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+
+      <TableCell className="font-medium">
+        {order.total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </TableCell>
+
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 size-3" />
